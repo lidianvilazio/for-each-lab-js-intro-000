df02017b581594fa90cce29c3bb0ca29143dd828
Lidian Vilazio <lidianvilazio@hotmail.com>
2018-01-04 20:45:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,3 @@ function iterativeLog(array) {
   dex}: ${element}`)
   })
 }
-
-function iterate(callback) {
-  var array = [1,2,3,4,5]
-  array.forEach(callback)
-  return array
-}
-
-function doToArray(array, callback) {
-  array.forEach(callback);
-}
